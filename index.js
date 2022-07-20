da21167fb12a21b5d732a4bc125129548ceef650
grantaspetraitis <grantas157@gmail.com>
2022-07-20 21:01:17 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const initDB = require('./db').initDB;
+const cors = require('cors');
 
 const app = express();
 
@@ -8,6 +9,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors({
+    origin: 'https://synthusiast-frontend.vercel.app'
+    
+}))
 
 initDB();
 
